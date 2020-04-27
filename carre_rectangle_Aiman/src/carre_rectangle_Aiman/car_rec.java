8f87c62fd69acfde442abf39d370c6c5c0b86818
aiman houssein <aiman10@outlook.fr>
2020-04-27 15:20:45 +0200

    JAVA à rendre

---
+++ carre_rectangle_Aiman/src/carre_rectangle_Aiman/car_rec.java
@@ -0,0 +1,22 @@
+package carre_rectangle_Aiman;
+
+import java.util.Scanner;
+
+public class car_rec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double la,lo,s,p;
+		Scanner lire=new Scanner(System.in);
+		System.out.println("entrer la longueur");
+		lo=lire.nextDouble();
+		System.out.println("entrer la largeur");
+		la=lire.nextDouble();
+		p=(la+lo)*2;
+		s=la*lo;
+		System.out.println("entrer la surface est"+s);
+		System.out.println("le perimetre est"+p);
+
+	}
+
+}
